884e317fc19507ef331a615272ea610b29c87a75
seanc0der <sean.noko@gmail.com>
2023-08-12 21:48:21 +0200

    updated the 'net-income-iwa9.js file's codebase by amending and updating the  codebase. It now produces the required output

--- net-income-iwa9.js
+++ net-income-iwa9.js
@@ -1,37 +1,37 @@
 const salary = 4000;
-const lodging = 'apartment'
-const size = 'large'
-
+const lodging = "apartment";
+const size = "large";
 
 // Only change the syntax below (not the values or key names)
 
 const expenses = {
-    food: 51.7501,
-    transport:  10.2,
-}
-  
-const tax = {
-    734: '3%',
-    234: '20%',
-    913: '12%',
-    415: '38%',
-    502: '42%',
-}
+	food: 51.7501,
+	transport: 10.2,
+};
 
-const rent = {
-    none: 0,
-    'small-room': 200,
-    'large-room': 300,
-    'small-apartment': 400,
-    'large-apartment': 800,
-    'small-house': 1200,
-    'large-house': 2400,
-}
+const tax = {
+	734: "3%",
+	234: "20%",
+	913: "12%",
+	415: "38%",
+	502: "42%",
+};
+
+expenses.rent = {
+	none: 0,
+	"small-room": 200,
+	"large-room": 300,
+	"small-apartment": 400,
+	"large-apartment": 800,
+	"small-house": 1200,
+	"large-house": 2400,
+};
 
 // You can change below however you want
 
-const taxAsDecimal = tax.913 / 100
-const startingAfterTax = salary * 1 - taxAsDecimal
-const type = lodging + size
-const balance = expenses(transport) - expenses(food) - expenses(rent.type) 
-console.log(balance)
+const taxAsDecimal = Number(tax[913].replace("%", "")) / 100;
+const salaryAfterTax = salary * (1 - taxAsDecimal);
+const type = `${size}-${lodging}`;
+const totalExpenses = expenses.transport + expenses.food + expenses.rent[type];
+const balance = salaryAfterTax - totalExpenses;
+console.log(balance.toFixed(2));
